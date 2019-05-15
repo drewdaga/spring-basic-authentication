1054fd3e8b5f56af175941de8ff9dcbf4aa6deec
Drew Daga <drew@drewdaga.com>
2019-05-15 19:28:44 -0400

    Simple Bootstrap Spring Boot with Thymeleaf running

---
+++ src/main/java/com/drewdaga/springbootdemo/SimpleController.java
@@ -0,0 +1,22 @@
+package com.drewdaga.springbootdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by amcgalliard on 5/15/2019.
+ */
+@Controller
+public class SimpleController {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
